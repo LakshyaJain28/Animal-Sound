d995b1b9ad968ac898bac2853c2cd3dbda8990bb
LakshyaJain28 <79844523+LakshyaJain28@users.noreply.github.com>
2021-10-19 18:23:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,3 +8,40 @@ function modelReady()
 {
     classifier.classify(gotResults);
 }
+function gotResults(error,results)
+{
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        random_number_r= Math.floor(Math.random()*255)+1;
+        random_number_g= Math.floor(Math.random()*255)+1;
+        random_number_b= Math.floor(Math.random()*255)+1;
+
+        document.getElementById("result_label").innerHTML="I Can Hear -"+results[0].label;
+        document.getElementById("result_confidence").innerHTML="Accuracy -"+(results[0].confidence*100).toFixed(2)+" %";
+        document.getElementById("result_label").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+        document.getElementById("result_confidence").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+    img=document.getElementById("alien1");
+   img1=document.getElementById("alien2");
+   img2=document.getElementById("alien3");
+   img3=document.getElementById("alien4");
+
+   if(results[0].label=='Barking'){
+    img.src='dog.png';
+}
+else if(results[0].label=='Meowing'){
+    img.src='cat.jpg';
+}
+else if(results[0].label=='Mooing'){
+   img.src='cow.jpg';
+}
+else if(results[0].label=='Trumpet'){
+    img.src='elephant.jpg';
+ }
+else {
+    img.src='';
+}
+ }    
+}
